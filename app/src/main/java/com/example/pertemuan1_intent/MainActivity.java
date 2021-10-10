7a939f2daa307481bd8c204ed0f3765cc32719ef
unknown <sk712851@gmail.com>
2021-10-10 11:14:26 +0700

    first commit

---
+++ app/src/main/java/com/example/pertemuan1_intent/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pertemuan1_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.pertemuan1_intent.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void biodata(View view) {
+        Intent a = new Intent(MainActivity.this, biodata.class);
+        startActivity(a);
+    }
+    public void segitiga(View view) {
+        Intent a = new Intent(MainActivity.this, segitiga.class);
+        startActivity(a);
+
+    }
+}
